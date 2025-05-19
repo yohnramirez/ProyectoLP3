f1d1ac1addc0b6c5a285783f08ac89e754bfe3dd
yohnramirez <yohnramirezsilva7@gmail.com>
2025-05-19 01:03:15 -0500

    Update ProjectApplication

--- Backend/project/src/main/java/com/backend/project/ProjectApplication.java
+++ Backend/project/src/main/java/com/backend/project/ProjectApplication.java
@@ -18,5 +18,8 @@ public class ProjectApplication {
 		System.setProperty("URL_DB", dot.get("URL_DB"));
 		System.setProperty("USER_DB", dot.get("USER_DB"));
 		System.setProperty("PASSWORD_DB", dot.get("PASSWORD_DB"));
+		System.setProperty("AUTH_SUPABASE_URL", dot.get("AUTH_SUPABASE_URL"));
+		System.setProperty("AUTH_SUPABASE_KEY", dot.get("AUTH_SUPABASE_KEY"));
+		System.setProperty("SUPABASE_SERVICE_ROLE_KEY", dot.get("SUPABASE_SERVICE_ROLE_KEY"));
 	}
 }
